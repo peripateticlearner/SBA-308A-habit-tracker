4971fdb7947a56bbd033c7e9488ca1672bb085e0
Ali Kirat <91100475+peripateticlearner@users.noreply.github.com>
2025-02-25 20:57:21 -0800

    Fixed error logging and improved the function return values

--- js/api.js
+++ js/api.js
@@ -1,28 +1,50 @@
 const API_URL = "https://67bac75dfbe0387ca1381f12.mockapi.io/api/v1/habits"; 
 
 async function fetchHabits() {
-    const response = await fetch(API_URL);
-    return response.json();
+    try {
+        const response = await fetch(API_URL);
+        return await response.json();
+    } catch (error) {
+        console.error("Error fetching habits:", error);
+        return [];
+    }
 }
 
 async function addHabit(title) {
+   try {
     const response = await fetch(API_URL, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ title, completed: false })
     });
-    return response.json();
+    return await response.json();
+   } catch (error) {
+    console.error("Error adding habit:", error);
+    return null;
+   } 
 }
 
 async function updateHabit(id, completed) {
-    const response = await fetch(`${API_URL}/${id}`, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ completed })
-    });
-    return response.json();
+    try {
+        const response = await fetch(`${API_URL}/${id}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ completed })
+        });
+        return await response.json();
+    } catch (error) {
+        console.error(`Error updating habit(ID: ${id}):`, error);
+        return null;
+        
+    }
+    
 }
 
 async function deleteHabit(id) {
-    await fetch(`${API_URL}/${id}`, { method: "DELETE" });
+    try {
+        await fetch(`${API_URL}/${id}`, { method: "DELETE" });
+    } catch (error) {
+        console.error(`Error deleting habit(ID: ${id}):`, error);
+        return null;
+    } 
 }
